dffba9e5a9d40b3bece08f2c764afdfb3afe8834
Dhruv <pateldhruv5536@gmail.com>
2024-06-17 17:08:25 +0530

    complete

--- script.js
+++ script.js
@@ -38,7 +38,20 @@ function setupIntersectionObserver(element, isLTR, speed) {
 const line1 = document.getElementById("line1");
 const line2 = document.getElementById("line2");
 const line3 = document.getElementById("line3");
-
+const line4 = document.getElementById("line4")
 setupIntersectionObserver(line1, true, 0.15);
 setupIntersectionObserver(line2, false, 0.15);
 setupIntersectionObserver(line3, true, 0.15);
+setupIntersectionObserver(line4, true, 0.8);
+
+
+const dtELements= document.querySelectorAll('dt')
+dtELements.forEach(element =>{
+    element.addEventListener('click',()=>{
+        const ddId =element.getAttribute('aria-controls')
+            const ddElement = document.getElementById(ddId)
+            const ddArrowIcon =element.querySelectorAll('i')[0]
+            ddElement.classList.toggle('hidden')
+            ddArrowIcon.classList.toggle('-rotate-180')
+    })
+})
